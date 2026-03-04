ec387c193b74dd74efc0fa4c047f6d932b37b066
Sean Peacock <s.peacock@verizon.net>
2026-03-04 14:01:27 -0500

    Fixes and formatting

--- main.js
+++ main.js
@@ -1,95 +1,91 @@
-var canvas;
-var gl;
-var program;
+let canvas;
+let gl;
+let program;
 
-var numTimesToSubdivide = 4;
+let numTimesToSubdivide = 4;
 
-var index = 0;
+let index = 0;
 
-var necleusPosition = vec3[0,0,0];
+let nucleusPosition = vec3[0, 0, 0];
 
-var electronPositions=[vec3(0,0,0),vec3(5,0,0),vec3(-5,0,0)];
+let electronPositions = [vec3(0, 0, 0), vec3(5, 0, 0), vec3(-5, 0, 0)];
 
-var electronTransform;
-var isAnimating= true;
-var theta = 0;
+let electronTransform;
+let isAnimating = true;
+let theta = 0;
 
-var pointsArray = [];
-var normalsArray = [];
+let pointsArray = [];
+let normalsArray = [];
 
 //Make sure these are set properly, 
 //or sphere could appear black
-var near = 0.1;
-var far = 100;
+let near = 0.1;
+let far = 100;
 
-var left = -10;
-var right = 10;
-var ytop =10;
-var bottom = -10;
+let left = -10;
+let right = 10;
+let ytop = 10;
+let bottom = -10;
 
-var va = vec4(0.0, 0.0, -1.0,1);
-var vb = vec4(0.0, 0.942809, 0.333333, 1);
-var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-var vd = vec4(0.816497, -0.471405, 0.333333,1);
+let va = vec4(0.0, 0.0, -1.0, 1);
+let vb = vec4(0.0, 0.942809, 0.333333, 1);
+let vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+let vd = vec4(0.816497, -0.471405, 0.333333, 1);
 
-var lightPosition = vec4(1.0, 0.0, 10.0, 1.0);  //eye coordinates
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
-var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
-var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+let lightPosition = vec4(1.0, 0.0, -1.0, 1.0);  //eye coordinates
+let lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+let lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
-var materialDiffuse = vec4( 1.0, 1.0, 0.0, 1.0 );
-var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
-var materialShininess = 20.0;
+let materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
+let materialDiffuse = vec4(1.0, 1.0, 0.0, 1.0);
+let materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+let materialShininess = 20.0;
 
-var modelViewMatrix, projectionMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc;
+let modelViewMatrix, projectionMatrix;
+let modelViewMatrixLoc, projectionMatrixLoc;
 
-var eye;
-var at = vec3(0.0, 0.0, 0.0);
-var up = vec3(0.0, 1.0, 0.0);
+let eye;
+let at = vec3(0.0, 0.0, 0.0);
+let up = vec3(0.0, 1.0, 0.0);
 
 function triangle(a, b, c) {
 
 
+    pointsArray.push(a);
+    pointsArray.push(b);
+    pointsArray.push(c);
 
-     pointsArray.push(a);
-     pointsArray.push(b);
-     pointsArray.push(c);
+    // normals are vectors but where w = 0.0,
+    // since normals do not have a homogeneous coordinate
+    normalsArray.push(a[0], a[1], a[2], 0.0);
+    normalsArray.push(b[0], b[1], b[2], 0.0);
+    normalsArray.push(c[0], c[1], c[2], 0.0);
 
-     // normals are vectors but where w = 0.0,
-     // since normals do not have a homogeneous coordinate
-     normalsArray.push(a[0],a[1], a[2], 0.0);
-     normalsArray.push(b[0],b[1], b[2], 0.0);
-     normalsArray.push(c[0],c[1], c[2], 0.0);
-
-     index += 3;
+    index += 3;
 
 }
 
 
 function divideTriangle(a, b, c, count) {
-    if ( count > 0 ) {
+    if (count > 0) {
 
-        var ab = mix( a, b, 0.5);
-        var ac = mix( a, c, 0.5);
-        var bc = mix( b, c, 0.5);
+        let ab = mix(a, b, 0.5);
+        let ac = mix(a, c, 0.5);
+        let bc = mix(b, c, 0.5);
 
         ab = normalize(ab, true);
         ac = normalize(ac, true);
         bc = normalize(bc, true);
 
-        divideTriangle( a, ab, ac, count - 1 );
-        divideTriangle( ab, b, bc, count - 1 );
-        divideTriangle( bc, c, ac, count - 1 );
-        divideTriangle( ab, bc, ac, count - 1 );
-    }
-    else {
-        triangle( a, b, c );
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(ab, b, bc, count - 1);
+        divideTriangle(bc, c, ac, count - 1);
+        divideTriangle(ab, bc, ac, count - 1);
+    } else {
+        triangle(a, b, c);
     }
 }
-
-
 
 
 function tetrahedron(a, b, c, d, n) {
@@ -99,51 +95,51 @@ function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, c, d, n);
 }
 
-function handleKey(evt){
+function handleKey(evt) {
     console.log("a");
-    var key = evt.key.toLowerCase();
+    let key = evt.key.toLowerCase();
 
-    if (key=="a"){
-        isAnimating=!isAnimating;
-    }
-    else if(key=="s"){
-        
-    }
-    else if(key=="l"){
+    if (key == "a") {
+        isAnimating = !isAnimating;
+    } else if (key == "s") {
 
-    }
-    else if (key == "t"){
+    } else if (key == "l") {
+
+    } else if (key == "t") {
 
     }
 
 }
+
 window.onload = function init() {
 
-    canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById("gl-canvas");
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
     gl.enable(gl.DEPTH_TEST);
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    
+
     window.addEventListener("keydown", handleKey);
 
     tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
 
     //Pass in vertex data
-   
+
 
     //Pass in transformation matrices
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
 
     //Pass in parameters for lighting equations
     gl.uniform4fv(gl.getUniformLocation(program, "lightDiffuse"), flatten(lightDiffuse));
@@ -162,48 +158,64 @@ window.onload = function init() {
 
 function render() {
 
-    if (isAnimating){theta+=0.5;}
-    
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    if (isAnimating) {
+        theta += 0.5;
+    }
 
-    eye = vec3(0, 0, 1.5);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    modelViewMatrix = lookAt(eye, at , up);
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    eye = vec3(0, 0, 10.0);
 
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
-    
+    modelViewMatrix = lookAt(eye, at, up);
+    projectionMatrix = perspective(90, 1, near, far);
 
-for (var j =0; j<electronPositions.length; j++){
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
 
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
 
-    //Pass in normal data
-    var vNormal = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vNormal);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+    drawElectrons();
+    //drawNucleus();
 
-    var vNormalPosition = gl.getAttribLocation( program, "vNormal");
-    gl.vertexAttribPointer(vNormalPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vNormalPosition);
-   
-
-    translation=translate(electronPositions[j][0],electronPositions[j][1],electronPositions[j][2]);
-    var translationU = gl.getUniformLocation(program, "translation");
-    gl.uniformMatrix4fv(translationU, false, flatten(translation));
-
-    electronTransform = rotateZ(theta);
-    var electronTransformU = gl.getUniformLocation(program, "electronTransform");
-    gl.uniformMatrix4fv(electronTransformU, false, flatten(electronTransform));
-
-    gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
-    
+    requestAnimFrame(render);
 }
-requestAnimFrame(render);
+
+function buildNucleus() {
+
+}
+
+function drawElectrons() {
+    for (let j = 0; j < electronPositions.length; j++) {
+        let vBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+
+        let vPosition = gl.getAttribLocation(program, "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+
+        //Pass in normal data
+        let vNormal = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vNormal);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+        let vNormalPosition = gl.getAttribLocation(program, "vNormal");
+        gl.vertexAttribPointer(vNormalPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vNormalPosition);
+
+
+        translation = translate(electronPositions[j][0], electronPositions[j][1], electronPositions[j][2]);
+        let translationU = gl.getUniformLocation(program, "translation");
+        gl.uniformMatrix4fv(translationU, false, flatten(translation));
+
+        electronTransform = rotateZ(theta);
+        let electronTransformU = gl.getUniformLocation(program, "electronTransform");
+        gl.uniformMatrix4fv(electronTransformU, false, flatten(electronTransform));
+
+        gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+
+    }
+}
+
+function drawNucleus() {
+
 }
