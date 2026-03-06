34dc479cf4fc273e49be65cde8c339954439a806
Daniel Gomes <112955696+GoldenEagle765@users.noreply.github.com>
2026-03-06 13:20:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -468,10 +468,11 @@ function loadImage(file, onload) {
   return image;
 }
 
-function loadTexture(url) {
+function loadTexture(file) {
+  gl.activeTexture(gl.TEXTURE11);
   const texture = gl.createTexture();
   const image = new Image();
-
+  image.crossOrigin = "";
   gl.bindTexture(gl.TEXTURE_2D, texture);
 
   gl.texImage2D(
@@ -485,17 +486,6 @@ function loadTexture(url) {
     gl.UNSIGNED_BYTE,
     new Uint8Array([255, 255, 255, 255])
   );
-
-  image.onload = function () {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  };
 
   image.src = url;
   return texture;
@@ -969,4 +959,5 @@ function pushSphere() {
 
   gl.vertexAttribPointer(vTexCoordLoc, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vTexCoordLoc);
+
 }
