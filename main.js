82605910d7f21ffdef627fccc698d0835684860a
Sean Peacock <s.peacock@verizon.net>
2026-03-06 12:19:14 -0500

    Added reflections

--- main.js
+++ main.js
@@ -264,6 +264,55 @@ function handleKey(evt) {
   evt.preventDefault();
 }
 
+function configureDefaultCubeMap() {
+  let cubeMap = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE8);
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeMap);
+
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  let red = new Uint8Array([255, 0, 0, 255]);
+  let green = new Uint8Array([0, 255, 0, 255]);
+  let blue = new Uint8Array([0, 0, 255, 255]);
+  let cyan = new Uint8Array([0, 255, 255, 255]);
+  let magenta = new Uint8Array([255, 0, 255, 255]);
+  let yellow = new Uint8Array([255, 255, 0, 255]);
+
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, red);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, yellow);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, green);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, cyan);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, blue);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, magenta);
+
+  gl.uniform1i(gl.getUniformLocation(program, "texMap"), 8);
+}
+
+function configureCubeMap(image) {
+  let cubeMap = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE8);
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeMap);
+
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+  gl.uniform1i(gl.getUniformLocation(program, "texMap"), 8);
+}
+
 function configureDefaultSkybox() {
 
   let tex = gl.createTexture();
@@ -516,6 +565,7 @@ window.onload = function init() {
   buildNucleus();
 
   modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+  modelViewInverseMatrixLoc = gl.getUniformLocation(program, "modelViewInverseMatrix");
   projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
 
   translationLoc = gl.getUniformLocation(program, "translation");
@@ -555,7 +605,9 @@ window.onload = function init() {
   colorCube();
 
   configureDefaultSkybox();
+  configureDefaultCubeMap();
   loadImage("textures/skybox.jpg", configureSkybox);
+  loadImage("textures/sun.webp", configureCubeMap);
 
  vTexCoordLoc = gl.getAttribLocation(program, "vTexCoord");
  uUseTextureLoc = gl.getUniformLocation(program, "uUseTexture");
@@ -611,6 +663,7 @@ function render() {
     gl.uniform1i(uShadowPassLoc, 1);
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(mat4()));
+    gl.uniformMatrix4fv(modelViewInverseMatrixLoc, false, flatten(mat4()));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(mat4()));
 
     gl.disable(gl.BLEND);
@@ -653,6 +706,7 @@ function render() {
   gl.uniform1i(uShadowPassLoc, 0);
 
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.uniformMatrix4fv(modelViewInverseMatrixLoc, false, flatten(inverse(modelViewMatrix)));
   gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
 
   if (uShadowsEnabledLoc) gl.uniform1i(uShadowsEnabledLoc, (shadowsEnabled && lightingEnabled) ? 1 : 0);
